abc67f10372e7539833074fa0238a6ddea6b471c
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-07-15 04:36:45 +0300

    add git ignore

---
+++ src/test/java/com/springdemo/springexercises/SpringExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.springexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
